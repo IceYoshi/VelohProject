6c7fed1f42259d19483e0f68cf14bcbc1aacea89
IceYoshi <mike_pereira100000@yahoo.de>
2016-12-12 22:30:53 +0100

    Implement About Activity (still WIP)

--- app/src/main/java/lu/mike/uni/velohproject/AboutActivity.java
+++ app/src/main/java/lu/mike/uni/velohproject/AboutActivity.java
@@ -3,13 +3,25 @@ package lu.mike.uni.velohproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        View aboutPage = new AboutPage(this)
+                .setImage(R.drawable.velho)
+                .setDescription(getResources().getString(R.string.ABOUT_DESCRIPTION))
+                .addGroup(getResources().getString(R.string.ABOUT_AUTHORS))
+                .addItem(createElementAutor(R.string.ABOUT_MIKE_NAME, R.string.ABOUT_MIKE_EMAIL))
+                .addItem(createElementAutor(R.string.ABOUT_DREN_NAME, R.string.ABOUT_DREN_EMAIL))
+                .addGitHub(getResources().getString(R.string.ABOUT_GITHUB))
+                .create();
+        setContentView(aboutPage);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -22,5 +34,12 @@ public class AboutActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public Element createElementAutor(int nameID, int emailID) {
+        Element elem = new Element();
+        elem.setTitle(getResources().getString(nameID) + "\n" + getResources().getString(emailID));
+        elem.setIcon(R.drawable.ic_person);
+        return elem;
     }
 }
